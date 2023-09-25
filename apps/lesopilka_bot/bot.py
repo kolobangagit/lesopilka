6d5be8faec40c264847c1dada99015b65e3a5894
berluskuni <berluskuni@gmail.com>
2023-09-25 16:28:09 +0300

    bot_2

--- apps/lesopilka_bot/bot.py
+++ apps/lesopilka_bot/bot.py
@@ -1,54 +1,27 @@
 import asyncio
 import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils.executor import start_webhook
-
-API_TOKEN = '6569461259:AAEu8HfH6vohmD3QK93CUkDKX6D5nLuRzV4'
-
-# webhook settings
-WEBHOOK_HOST = 'https://lesopilka-kirov.ru'
-WEBHOOK_PATH = '/lesopilka-bot'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-
-# webserver settings
-WEBAPP_HOST = '127.0.0.1'  # or ip
-WEBAPP_PORT = 3001
-
+# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="6569461259:AAEu8HfH6vohmD3QK93CUkDKX6D5nLuRzV4")
+# Диспетчер
+dp = Dispatcher()
 
-loop = asyncio.get_event_loop()
-bot = Bot(token=API_TOKEN, loop=loop)
-dp = Dispatcher(bot)
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+# Хэндлер на команду /start
 
 
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-    # insert code here to run it after start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
 
 
-async def on_shutdown(dp):
-    # insert code here to run it before shutdown
-    logging.warning('Shutting down..')
+async def main():
+    await dp.start_polling(bot)
 
-    # insert code here to run it before shutdown
-
-    # Remove webhook (not acceptable in some cases)
-    await bot.delete_webhook()
-
-    # Close DB connection (if used)
-    # await dp.storage.close()
-    # await dp.storage.wait_closed()
-
-    logging.warning('Bye!')
-
-
-if __name__ == '__main__':
-    start_webhook(dispatcher=dp, webhook_path=WEBHOOK_PATH, on_startup=on_startup, on_shutdown=on_shutdown,
-                  skip_updates=True, host=WEBAPP_HOST, port=WEBAPP_PORT)
+if __name__ == "__main__":
+    asyncio.run(main())
