5fe0b5a246145a578f233c3dd10aa2c0dea7cff2
berluskuni <berluskuni@gmail.com>
2023-09-26 14:52:20 +0300

    26-09-2023_2 kolobanga

--- apps/kolobanga/urls.py
+++ apps/kolobanga/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from apps.kolobanga.views import send_bot_info
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="kolobanga/home.html"))
+    path('', TemplateView.as_view(template_name="kolobanga/home.html")),
+    path('send_info/', send_bot_info, name="send_bot_info")
 ]
--- apps/kolobanga/views.py
+++ apps/kolobanga/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+
+TELEGRAM_URL = "https://api.telegram.org/bot"
+TOKEN = '6569461259:AAEu8HfH6vohmD3QK93CUkDKX6D5nLuRzV4'
+
+
+def send_bot_info(request):
+    if request.method == 'POST' and request.is_ajax():
+        name = request.POST.get('name', None)
+        phone = request.POST.get('phone', None)
+        message = f'Имя: {name} \n Телефон: {phone}'
+        data = {
+            "chat_id": 6027445072,
+            "text": message,
+        }
+        response = requests.get(
+            f"{TELEGRAM_URL}{TOKEN}/sendMessage", data=data
+        )
+        return JsonResponse({'status': True})
+    else:
+        return JsonResponse({'status': False})
 
 # Create your views here.
